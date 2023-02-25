3d94b56b408278cb85fa50853127f4e918f1f272
top <91580790+Minagoroshi@users.noreply.github.com>
2023-02-25 00:32:50 -0500

    Added start and stop message with elapsed time for verbosity

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,12 @@ fn main() {
     }
 
     let path = Path::new(path);
-
+    println!("Starting Search in {:?} for \"{}\"", path, search_string);
+    let start = std::time::Instant::now();
     walk_dir(&path, &search_string, case_sensitive);
+    let end = std::time::Instant::now();
+    let elapsed = end - start;
+    println!("Search took {} seconds", elapsed.as_secs());
 }
 
 fn walk_dir(path: &Path, search_string: &str, case_sensitive: bool) {
